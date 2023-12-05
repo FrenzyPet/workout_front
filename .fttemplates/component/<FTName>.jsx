38b445bef2716e0f18464f7347dcab519159c51e
frenzy <manpatbro@gmail.com>
2023-12-05 23:34:34 +0300

    base project setup

---
+++ .fttemplates/component/<FTName>.jsx
@@ -0,0 +1,13 @@
+
+import s from './<FTName>.module.scss'
+
+const <FTName | pascalcase> = () => {
+	return (
+		<div className={s.main}></div>
+	);
+};
+
+
+export default <FTName | pascalcase>
+
+
---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import s from './app.module.scss'
+
+const App = () => {
+	return (
+		<div className={s.main}>
+			<div></div>
+		</div>
+	)
+}
+
+export default App
