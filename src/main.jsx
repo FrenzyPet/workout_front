d8eab4ce645bc556d2a2bf40f84017830154895d
frenzy <manpatbro@gmail.com>
2023-12-06 22:22:49 +0300

    setup base styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/app'
 
-import './index.css'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
